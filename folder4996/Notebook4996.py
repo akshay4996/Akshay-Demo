2bf93e909e146c8fdccbad8c88cc1f8301af8b3d
akshay4996 <akshay4996>
2023-09-24 13:48:01 +0000

    Testing book

---
+++ folder4996/Notebook4996.py
@@ -0,0 +1,19 @@
+# Databricks notebook source
+a = 'hello akshay'
+print(a)
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select 5+5, 'akshay'
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC #akshay
+# MAGIC ##rajesh
+# MAGIC ###salunke
+
+# COMMAND ----------
+
+
